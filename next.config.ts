20156b271352f61998f0adbd7942cb844e869919
Karan Hinduja <hinduja1988@gmail.com>
2026-08-05 18:13:41 +0530

    chore: setup Dockerfile and next config for Phase 2 ship

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/mbrdi-onsite-session',
+  assetPrefix: '/mbrdi-onsite-session',
+  output: 'standalone',
 };
 
 export default nextConfig;
